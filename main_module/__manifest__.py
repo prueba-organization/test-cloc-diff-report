f7cbd4c627bd116a1029989c979b78897d347114
Mauricio <mauricio@vauxoo.com>
2017-11-28 13:22:38 -0400

    add __manifest__.py

---
+++ main_module/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Test cloc',
+    'version': '11.0.0.0.0',
+    'category': 'Tools',
+    'author': 'Mauricio',
+    'license': 'LGPL-3',
+    'website': 'https://www.vauxoo.com',
+    'depends': [],
+    'demo': [],
+    'test': [],
+    'auto_install': False,
+    'installable': False,
+}
